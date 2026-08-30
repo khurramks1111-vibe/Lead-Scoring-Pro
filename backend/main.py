a60f5204c154699e2eb85f74d82d6c120f415c05
Khurram Shahzad <khurramks1111@gmail.com>
2026-08-30 10:38:24 +0300

    Restrict allowed HTTP methods in CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -24,8 +24,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 def get_db():
